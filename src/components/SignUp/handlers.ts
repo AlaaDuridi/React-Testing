3bc9e97917aca2393158cc413e54fa13b604d038
Alaa Duridi <alla_duridi@hotmail.com>
2024-11-18 00:55:35 +0200

    Use msw to mock the necessary api

--- src/components/SignUp/handlers.ts
+++ src/components/SignUp/handlers.ts
@@ -1,7 +1,20 @@
 import { http, HttpResponse } from "msw";
 
+interface Response {
+  token: string;
+  message: string;
+}
+
 export const handlers = [
-  http.post("*/register", () => {
-    return HttpResponse.json({});
+  http.post("*/register", async ({ request }) => {
+    const body = (await request.json()) as Response;
+
+    if (!body.token)
+      return HttpResponse.json(
+        { error: "Internal Server Error" },
+        { status: 400 },
+      );
+
+    return HttpResponse.json(body);
   }),
 ];
